2953eb22d1eb31dbd6dce038494bb321dcd0d885
Guy Tevet <guy.tvt@gmail.com>
2025-01-19 09:22:59 +0200

    Update download_t2m_evaluators.sh; Fixed #229

--- prepare/download_t2m_evaluators.sh
+++ prepare/download_t2m_evaluators.sh
@@ -1,6 +1,8 @@
 echo -e "Downloading T2M evaluators"
-gdown --fuzzy https://drive.google.com/file/d/1DSaKqWX2HlwBtVH5l7DdW96jeYUIXsOP/view
-gdown --fuzzy https://drive.google.com/file/d/1tX79xk0fflp07EZ660Xz1RAFE33iEyJR/view
+# gdown --fuzzy https://drive.google.com/file/d/1DSaKqWX2HlwBtVH5l7DdW96jeYUIXsOP/view
+# gdown --fuzzy https://drive.google.com/file/d/1tX79xk0fflp07EZ660Xz1RAFE33iEyJR/view
+gdown --fuzzy https://drive.google.com/file/d/1O_GUHgjDbl2tgbyfSwZOUYXDACnk25Kb/view
+gdown --fuzzy https://drive.google.com/file/d/12liZW5iyvoybXD8eOw4VanTgsMtynCuU/view
 rm -rf t2m
 rm -rf kit
 
